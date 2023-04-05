09124e936660b9f73e3957d93a578fdba4c74e13
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-04-05 22:53:00 +0500

    update

--- Includes/Header.php
+++ Includes/Header.php
@@ -39,8 +39,9 @@
                             All Services
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/Web-and-Seo-Experts/app-development.php">App Development</a></li>
+                            <li><a class="dropdown-item" href="/Web-and-Seo-Experts/app-development-services.php">App Development</a></li>
                             <li><a class="dropdown-item" href="/Web-and-Seo-Experts/seo-services.php">Search Engine Optimization</a></li>
+                            <li><a class="dropdown-item" href="/Web-and-Seo-Experts/google-ads-management-services.php">Google Ads Management</a></li>
                             <li><a class="dropdown-item" href="/Web-and-Seo-Experts/social-media-services.php">Social Media marketing</a></li>
                             <li><a class="dropdown-item" href="/Web-and-Seo-Experts/website-design-development-services.php">Website Design & Development</a></li>
                         </ul>
---
+++ google-ads-management-services.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include './Includes/head.php'; ?>
+    <title>Google Ads Management</title>
+</head>
+
+<body>
+    <?php include './Includes/header.php'; ?>
+
+    <div class="seo-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 banner-left d-flex align-items-center">
+                    <div>
+                        <h1>
+                            Google Ads Services To Turbocharge <span style="color: #fb8c00;">Your Online Presence</span>
+                        </h1>
+                        <p class="mb-3">
+                            At Web and SEO Experts, our team of Google Ads wizards will work their spellbinding charm to create and manage ad campaigns that are out of this world.
+                            We've got the magic touch to boost your Google ads and make your brand shine like a shooting star. </p>
+
+                        <button class="btns-one" type="submit">START PROJECT</button>
+                        <button class="btns-two" type="submit">CALL NOW</button>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                    <img src="./Assets/images/SEO-services.png" class="main-imgs">
+                </div>
+                <?php include './Includes/form.php' ?>
+                <div class="d-flex justify-content-center">
+                    <img src="./Assets/images/trustpilot-stacked-logo.png" alt="Trustpilot Stacked">&nbsp;&nbsp;&nbsp;
+                    <img src="./Assets/images/gpartner.png" alt="G Partner">&nbsp;&nbsp;&nbsp;
+                    <img src="./Assets/images/binsider.png" alt="B Insider">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row my-5">
+            <div class="col-md-6 text-center">
+                <img src="./Assets/images/socialMedia.png" alt="second-left-img" class="Images-main">
+            </div>
+            <div class="col-md-6 pt-5 right-side-panel">
+                <h2>
+                    Master Google AdWords With Advanced Keyword Research
+                </h2>
+                <p>
+                    Keywords are the lifeblood of any Google AdWords campaign. Guess what? Our advanced keyword research will help you become a true AdWords ninja and target your audience with precision. When it comes to conducting high-quality keyword research and discovering the ideal keywords to attract your ideal customers, we have everything we need at our disposal. Whether you're looking to boost your click-through rates or increase your conversions, we're here to help you slice through the competition. So what are you waiting for? Let's get this Google AdWords party started and conquer the digital marketing world together! </p>
+                <button class="btns-three" type="submit">START PROJECT</button>
+            </div>
+        </div>
+    </div>
+    <?php include './Includes/ctas.php'; ?>
+    <!---------------- Second Section Start ---------------->
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-md-12 pt-5 right-side-panel text-center">
+                <h2 class="mb-4">
+                    Compelling Google Ads Copy: Drive Conversions With Persuasive Messaging
+                </h2>
+                <p>
+                    For best results, your Google ads need to stand out from the competition and persuade potential customers to take action. That's where our team of expert copywriters comes in! Copywriting is an integral part of our Google ads management services. At Web and SEO Experts, we know how to craft ad copy that speaks directly to your audience, addresses their pain points, and entices them to click through to your website. We use persuasive messaging techniques that are proven to drive conversions, such as using social proof, creating a sense of urgency, and highlighting the unique benefits of your product or service. </p>
+                <button class="btns-three" type="submit">START PROJECT</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+
+            </div>
+        </div>
+    </div>
+    <!---------------- Second Section End ---------------->
+    <div class="container my-5">
+        <div class="row my-5">
+            <div class="col-md-6 text-center">
+                <img src="./Assets/images/Chatbot.png" alt="second-left-img" class="Images-main" width="500">
+            </div>
+            <div class="col-md-6 pt-5 right-side-panel">
+                <h2>
+                    Our Google Ads Management Services Leverage Data-Driven Insights
+                </h2>
+                <p>
+                    At Web and SEO Experts, we believe that data is the key to unlocking the full potential of optimal Google AdWords campaign management. Therefore, we collect and analyze data on a range of metrics, including click-through rates, conversion rates, and cost per click, to give you a clear picture of how your ads are performing. Our team of experts uses the insights we gain from analytics to optimize your Google ads, making adjustments to your targeted audience, ad copy, and bidding strategies as needed.
+                </p>
+                <button class="btns-three" type="submit">START PROJECT</button>
+            </div>
+        </div>
+    </div>
+    <?php include './Includes/ctas.php'; ?>
+    <div class="container my-5">
+        <div class="row my-5">
+            <div class="col-md-6 pt-5 right-side-panel">
+                <h2>
+                    Tailored Google Ads Services For Your Unique Needs
+                </h2>
+                <p>
+                    We don't believe in one-size-fits-all solutions. That's why we offer customized Google ads services that are as unique as your business. We'll work closely with you to understand your needs and develop a customized strategy that's designed to help you achieve your goals. And because we believe in transparency, we'll keep you informed every step of the way, providing you with regular updates and reports on your progress. <button class="btns-three" type="submit">START PROJECT</button>
+            </div>
+            <div class="col-md-6 text-center">
+                <img src="./Assets/images/paynow.jpg" alt="second-left-img" class="Images-main">
+            </div>
+        </div>
+    </div>
+
+
+    <!---------------- Fifth Section Start ---------------->
+    <div class="container py-5 my-5">
+        <div class="row">
+            <div class="col-md-12 text-center middle-text">
+                <h2>
+                    Our Process To Set Up Successful Google Ads Campaign
+                </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="tab d-flex mb-5 mobile-tab">
+                <button class="tablinks" onclick="openCity(event, 'Logo-Design')" id="defaultOpen">
+                    <i class="fa-solid fa-pen-nib"></i></button>
+                <button class="tablinks" onclick="openCity(event, 'Web-Design')"><i class="fa-solid fa-code"></i></button>
+                <button class="tablinks" onclick="openCity(event, 'Web-Development')"><i class="fa-brands fa-codepen"></i></button>
+                <button class="tablinks" onclick="openCity(event, 'Content-Writing')"><i class="fa-solid fa-folder-plus"></i></button>
+            </div>
+            <div class="tab d-flex mb-5 desk-tab">
+                <button class="tablinks" onclick="openCity(event, 'Logo-Design')" id="defaultOpenText">Understand Your Goals And Audience</button>
+                <button class="tablinks" onclick="openCity(event, 'Web-Design')">Conduct Keyword Research</button>
+                <button class="tablinks" onclick="openCity(event, 'Web-Development')"> Craft Compelling Ad Copy</button>
+                <button class="tablinks" onclick="openCity(event, 'Content-Writing')">Monitor And Optimize</button>
+            </div>
+            <div class="col-md-12">
+                <div id="Logo-Design" class="tabcontent">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <img src="./Assets/images/social-media-marketing.jpg" alt="features-logo-tips">
+                        </div>
+                        <div class="col-md-6 d-flex align-items-center">
+                            <div>
+                                <h3>
+                                    Understand Your Goals And Audience
+                                </h3>
+                                <p>To begin with, we dive deep into your business goals and identify your target audience to ensure we create a Google AdWords campaign that aligns with your objectives and resonates with your audience.</p>
+                                <button class="btns-six" type="submit">START PROJECT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="Web-Design" class="tabcontent">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <img src="./Assets/images/SMM.png" alt="features-logo-tips">
+                        </div>
+                        <div class="col-md-6 d-flex align-items-center">
+                            <div>
+                                <h3>
+                                    Conduct Keyword Research
+                                </h3>
+                                <p>Next up, we conduct comprehensive keyword research to identify the most effective and relevant keywords for your campaign, ensuring that your Google ads show up to the right people at the right time.</p>
+                                <button class="btns-six" type="submit">START PROJECT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="Web-Development" class="tabcontent">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <img src="./Assets/images/seo-marketing.png" alt="features-logo-tips">
+                        </div>
+                        <div class="col-md-6 d-flex align-items-center">
+                            <div>
+                                <h3>
+                                    Craft Compelling Ad Copy
+                                </h3>
+                                <p>Once we are done with keyword research, it's time to create unique ad copy that speaks directly to your target audience. </p>
+                                <button class="btns-six" type="submit">START PROJECT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="Content-Writing" class="tabcontent">
+                    <div class="row">
+                        <div class="col-md-6 text-center">
+                            <img src="./Assets/images/tool.png" alt="features-logo-tips">
+                        </div>
+                        <div class="col-md-6 d-flex align-items-center">
+                            <div>
+                                <h3>
+                                    Monitor And Optimize
+                                </h3>
+                                <p>Lastly, we continually monitor and optimize your campaign to ensure that it is performing at its best. Our team keeps a close eye on every detail of your campaign and tweaks it to get the best results possible. </p>
+                                <button class="btns-six" type="submit">START PROJECT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!---------------- Fifth Section End ---------------->
+    <?php include './Includes/ctas.php'; ?>
+
+    <div class="container my-5">
+        <div class="row my-5">
+            <div class="col-md-6 pt-5 right-side-panel">
+                <h2>
+                    What Makes Our Google Ads Agency Stand Out From The Competition?
+                </h2>
+                <p>
+                    By acquiring our Google ads management services, you can expect several perks such as:
+                </p>
+                <ul>
+                    <li>In-house copywriting, design, advertising, and development team</li>
+                    <li>A devoted account manager</li>
+                    <li>Customized monitoring and reporting integrations</li>
+                    <li>Fair and transparent pricing</li>
+                    <li>Proactively managing a campaign</li>
+                </ul>
+                <button class="btns-three" type="submit">START PROJECT</button>
+            </div>
+            <div class="col-md-6 text-center">
+                <img src="./Assets/images/fold2.png" alt="second-left-img" class="Images-main">
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row my-5">
+            <div class="col-md-6 text-center">
+                <img src="./Assets/images/digital-marketing-graphic.png" alt="second-left-img" class="Images-main">
+            </div>
+            <div class="col-md-6 pt-5 right-side-panel">
+                <h2>
+                    Bring In More Revenue
+                </h2>
+                <p>
+                    Web and SEO Experts is an industry-leading Google Ads agency, having managed over 650 paid Google advertising campaigns for clients in sectors as diverse as e-commerce, manufacturing, healthcare, and more. Working with us will ensure that your digital advertising campaign produces tangible results for your business.
+
+                </p>
+                <button class="btns-three" type="submit">START PROJECT</button>
+            </div>
+        </div>
+    </div>
+    <!---------------- Ninth Section Start ---------------->
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-md-12 text-center">
+                <h2 class="fw-bold">Ready, Set, Social! Select Your Desired Social Media Marketing Package Today</h2>
+                <p>We offer a range of social media marketing packages, so that you can choose the one that's right for you.</p>
+            </div>
+        </div>
+    </div>
+    <?php include './Includes/Pricing.php'; ?>
+    <!---------------- Ninth Section End ---------------->
+    <?php include './Includes/footer.php'; ?>
+
+</body>
+
+</html>
