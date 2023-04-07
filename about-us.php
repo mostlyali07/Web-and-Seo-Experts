637e438fdfc9f24d6d3e9ff38420aa525846c26b
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-04-07 14:52:07 +0500

    update

--- about-us.php
+++ about-us.php
@@ -35,6 +35,84 @@
             </div>
         </div>
     </div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-center">
+                <img src="./Assets/images/gogle-ads-12.jpg" alt="second-left-img" class="Images-main">
+            </div>
+            <div class="col-md-6 right-side-panel d-flex align-items-center">
+                <div>
+                    <h2>
+                        Our Mission – What We Strive To Achieve For Our Clients
+                    </h2>
+                    <p>
+                        At Web and SEO Experts, we don't just aim to be a run-of-the-mill digital marketing agency. Our mission is to be the catalyst for our client's success in the digital world, helping them achieve the results they desire with our innovative and dynamic approach to digital marketing. <br><br>
+                        We are passionate about helping businesses of all sizes unlock their full potential by leveraging the latest digital marketing strategies, tools, and technologies. <br><br>
+                        Our team of experts is constantly pushing the boundaries of what's possible, coming up with creative and outside-the-box solutions to help our clients stand out in their respective markets. Whether it's through creating visually stunning websites, developing engaging social media campaigns, or implementing targeted SEO strategies, we're committed to delivering outstanding results and driving growth for our clients.
+                    </p>
+                    <button class="btns-three" type="submit">START PROJECT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="gray">
+
+        <div class="container py-5">
+            <div class="row my-5">
+                <div class="col-md-6 right-side-panel d-flex align-items-center">
+                    <div class="about-us">
+                        <h2>
+                            The Core Values And Ethos We Follow At Web and SEO Experts
+                        </h2>
+                        <p>
+                            Our success is rooted in our core values and ethos. These principles guide our work and are central to everything we do. At Web and SEO Experts, we pride ourselves on our commitment to these values, as they reflect our dedication to delivering exceptional digital marketing services to our clients. <br><br>
+                            Here are three of our core values and what they mean to us:
+                        </p>
+
+                        <p>
+                        <h4><i class="fa-solid fa-hands-praying"></i> &nbsp;Integrity</h4>
+                        We believe that honesty and transparency are key to building strong and lasting relationships with our clients. We are committed to conducting ourselves with integrity in all aspects of our work, from the initial consultation to the final deliverables. Our team maintains open communication and delivers on its promises.
+                        </p>
+                        <p>
+                        <h4><i class="fa-solid fa-cloud-arrow-up"></i>&nbsp;Innovation</h4>
+                        We are passionate about staying ahead of the curve and using the latest digital marketing strategies, tools, and technologies to help our clients succeed. To better serve our clients' ever-changing requirements, our team is always exploring uncharted territory, trying out novel approaches, and digging into potential new avenues for improvement.
+                        </p>
+                        <p>
+                        <h4><i class="fa-solid fa-handshake"></i>&nbsp;Collaboration</h4>
+                        The best way to succeed is by partnering with our customers. Thus, we make it a point to take the time to understand their unique needs and goals and collaborate with them every step of the way to develop tailored solutions that meet their specific requirements. We work as a team, both internally and with our clients, to ensure that every project is a roaring success.
+                        </p>
+
+
+                        <button class="btns-three" type="submit">START PROJECT</button>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                    <img src="./Assets/images/about-us-02.png" alt="second-left-img" class="Images-main">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-center">
+                <img src="./Assets/images/gogle-ads-12.jpg" alt="second-left-img" class="Images-main">
+            </div>
+            <div class="col-md-6 right-side-panel d-flex align-items-center">
+                <div>
+                    <h2>
+                        Customer Testimonials: Hear Directly From Our Clients About Their Experiences Working With Web And SEO Experts
+                    </h2>
+                    <p>
+                        I am thrilled with the results I have seen from Web and SEO Experts! Their team of experts helped me revamp my website and increase my online visibility through SEO and social media marketing. My business has never been better, and I owe it all to the talented professionals at Web and SEO Experts.
+                        I had a great experience working with Web and SEO Experts on my web design and development project. Their team was responsive, creative, and detail-oriented throughout the entire process. The end result was a beautiful, user-friendly website that perfectly captures my brand's message and aesthetic.
+                        Web and SEO Experts has been an invaluable partner for my business's digital marketing needs. Their SEO strategies have helped me achieve top rankings on search engines, driving more traffic to my website and ultimately increasing my sales. I highly recommend their services to anyone looking to improve their online presence.
+                    </p>
+                    <button class="btns-three" type="submit">START PROJECT</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include './Includes/footer.php'; ?>
 </body>
 
